d32d5ba3e4c0fdeaf37cd642618ac066f12d3f1e
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2015-08-25 12:37:02 +0200

    Adjust exports

--- transit-module.scm
+++ transit-module.scm
@@ -4,9 +4,10 @@
 		 transit-decode
 		 current-transit-write-handlers
 		 current-transit-read-handlers
+		 make-tagged-value
 		 tagged-value?
 		 tagged-value-tag
-		 tagged-value-value
+		 tagged-value-ref
 		 make-write-handler)
   (import scheme chicken miscmacros matchable defstruct foreign)
   (use medea base64 data-structures srfi-1)
